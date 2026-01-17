b7961f2a14fb9c8e751cd8f766301c1424232d09
Velox <pwenxiang51@gmail.com>
2026-01-17 16:55:13 +0800

    update name

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SITE = {
   author: "Velo.x",
   profile: "https://satnaing.dev/",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "Velo.x 的博",
+  title: "Velo.x",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
